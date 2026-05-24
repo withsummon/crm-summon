c9b7e698c96cd1f06767f5ad17b63c2ff2d8838a
indradprasetya <indrayanadian7@gmail.com>
2026-05-24 18:56:24 +0800

    refactor: add stretch-left support to LayoutHeader

--- frontend/src/components/LayoutHeader.vue
+++ frontend/src/components/LayoutHeader.vue
@@ -1,25 +1,32 @@
+<script setup>
+import { ref, nextTick } from "vue";
+
+const props = defineProps({
+  stretchLeft: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const showHeader = ref(false);
+
+nextTick(() => {
+  showHeader.value = true;
+});
+</script>
+
 <template>
   <Teleport v-if="showHeader" to="#app-header">
     <slot>
-      <header
-        class="flex h-10.5 items-center justify-between py-[7px] sm:pl-5 pl-2"
-      >
-        <div class="flex items-center gap-2">
+      <header class="flex h-10.5 w-full items-center py-[7px] sm:pl-5 pl-2">
+        <div :class="['flex items-center gap-2', props.stretchLeft && 'flex-1']">
           <slot name="left-header" />
         </div>
-        <div class="flex items-center gap-2">
-          <slot name="right-header" class="flex items-center gap-2" />
+
+        <div class="ml-auto flex items-center gap-2">
+          <slot name="right-header" />
         </div>
       </header>
     </slot>
   </Teleport>
 </template>
-<script setup>
-import { ref, nextTick } from 'vue'
-
-const showHeader = ref(false)
-
-nextTick(() => {
-  showHeader.value = true
-})
-</script>
